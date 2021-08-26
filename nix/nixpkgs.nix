0ad04190e383011148c67af499f5e137b0b77f78
Sebastian Reuße <ser@foryouandyourcustomers.com>
2021-08-26 08:14:00 +0200

    tool: add Nix shell environment

---
+++ nix/nixpkgs.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchTarball {
+  # nixpkgs master @ 26-08-2021
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/491f953ce55f99fd0c4f63254e2d35a0b19f77c6.tar.gz";
+  sha256 = "1rzb2zjakj923fi7sjwv5qwrb4b828fy2rhxaxm1ab9mgwcky3vd";
+}) { }
---
+++ shell.nix
@@ -0,0 +1,2 @@
+let pkgs = import ./nix/nixpkgs.nix;
+in pkgs.mkShell { packages = with pkgs; [ gnumake go terraform_1_0_0 ]; }
