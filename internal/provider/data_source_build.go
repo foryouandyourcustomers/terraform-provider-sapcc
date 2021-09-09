1918c40e7c25b123359ef9e4aeab6223eb154dd2
Chandra Tungaturthi <tckb@tgrthi.me>
2021-09-09 11:20:57 +0200

    chore: making golint-ci happy ;)

--- internal/provider/data_source_build.go
+++ internal/provider/data_source_build.go
@@ -121,7 +121,9 @@ func (ds dataSourceBuild) Read(ctx context.Context, req tfsdk.ReadDataSourceRequ
 		resp.Diagnostics.Append(
 			diag.NewErrorDiagnostic(
 				"Provider not configured",
-				"The provider hasn't been configured before apply, likely because it depends on an unknown value from another resource. This leads to weird stuff happening, so we'd prefer if you didn't do that. Thanks!",
+				"The provider hasn't been configured before apply, "+
+					"likely because it depends on an unknown value from another resource. "+
+					"This leads to weird stuff happening, so we'd prefer if you didn't do that. Thanks!",
 			))
 
 		return
